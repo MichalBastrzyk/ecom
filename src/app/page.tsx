14bbd23d724068f4a4881c44bab52bac5feb6fd1
Michał Bastrzyk <michal.bastrzyk.pl@gmail.com>
2025-12-16 22:23:09 +0100

    tt

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,8 @@
-import { ComponentExample } from "@/components/component-example";
 
 export default function Page() {
-return <ComponentExample />;
+  return (
+    <div className="flex flex-col gap-10 py-10 items-center justify-center">
+        <h1 className="text-2xl font-bold">Welcome to the Ecom Dashboard</h1>
+    </div>
+  )
 }
